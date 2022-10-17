e591a69dcb4e63dd8da833893f253cc8fcd12316
Henrychang26 <henrychang26@gmail.com>
2022-10-16 22:41:13 -0700

    uniswapclone V1

---
+++ contracts/uniswap-clone.sol
@@ -0,0 +1,14 @@
+//SPDX-License-identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract UniswapClone {
+
+    event transactions();
+
+    function transfer (address _from, uint256 _amount)public payable {
+        require(_amount> 0);
+        
+
+    }
+}
