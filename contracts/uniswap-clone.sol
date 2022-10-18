bb9853e074098555476eb36ba97304133b753f1e
Henrychang26 <henrychang26@gmail.com>
2022-10-17 23:38:33 -0700

    uniswapclone V2

--- contracts/uniswap-clone.sol
+++ contracts/uniswap-clone.sol
@@ -1,13 +1,40 @@
-//SPDX-License-identifier: MIT
+//SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.0;
 
 contract UniswapClone {
 
-    event transactions();
+    //State Variables
 
-    function transfer (address _from, uint256 _amount)public payable {
-        require(_amount> 0);
+    uint256 public ethAmount;
+    address payable[] private clients;
+
+    error NoBalance();
+    error NotEnoughEth();
+
+    mapping (address => uint256) balanceOf;
+
+    event EthSent();
+    event EthStored(uint256 ethAmount);
+    event BalanceUpdated(address indexed clients);
+
+    function storeEth (uint256 _ethAmount) external payable {
+        if(msg.value <= 0){
+            revert NotEnoughEth();
+        }
+        _ethAmount = msg.value;
+        balanceOf[msg.sender] += msg.value;
+        clients.push(payable(msg.sender));
+        emit EthStored(_ethAmount);
+    }
+
+    function sendEth (uint256 _ethAmount)public payable {
+        _ethAmount = msg.value;
+        if(balanceOf[msg.sender] <= 0){
+            revert NoBalance();
+        }
+        balanceOf[msg.sender] = balanceOf[msg.sender] - _ethAmount;
+        emit BalanceUpdated(msg.sender);
         
 
     }
